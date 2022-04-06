2b37232b7304c1fe01c4725881e2946dcbbfbfa8
saba mahd <sabamahd.sm@gmail.com>
2022-04-06 18:48:40 -0400

    Server code started

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+// help fun
+const helpers = require()
+
+// handlebars
+
+// turn connection to db & server
